13c7d17def41028e35fba38bc243550314bcd05d
yw97alos <karol.bakas@fau.de>
2024-10-29 09:13:50 +0100

    me stupid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,9 @@
 <script lang="ts">
+	import '../app.css';
+	// export const trailingSlash = 'always';
+	// export const prerender = true;
+
 	let { children } = $props();
 </script>
 
-<nav class="navbar bg-base-100">
-	<div class="flex-1">
-		<a class="btn btn-ghost text-xl normal-case" href="/">Advent of School</a>
-	</div>
-	<div class="flex-none">
-		<ul class="menu menu-horizontal p-0">
-			<li><a href="/about">About</a></li>
-			<li><a href="/contact">Contact</a></li>
-		</ul>
-	</div>
-</nav>
-<main class="p-4">
-	{@render children()}
-</main>
+{@render children()}
