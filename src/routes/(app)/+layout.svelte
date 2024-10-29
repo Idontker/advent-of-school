16b79d411c42185fc7e0c6b0a694f8ac83e7c0a0
yw97alos <karol.bakas@fau.de>
2024-10-29 09:09:17 +0100

    preperation for static adapter

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+	// export const trailingSlash = 'always';
+	// export const prerender = true;
+
+	let { children } = $props();
+</script>
+
+{@render children()}
