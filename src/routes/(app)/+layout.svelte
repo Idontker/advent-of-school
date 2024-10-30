14de74e4944295a08fbbd07abbad0bc2f9c97bcb
yw97alos <karol.bakas@fau.de>
2024-10-30 01:11:03 +0100

    maybe remove dead links might help ?!

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -7,10 +7,10 @@
 		<a class="btn btn-ghost text-xl normal-case" href="/">Advent of School</a>
 	</div>
 	<div class="flex-none">
-		<ul class="menu menu-horizontal p-0">
+		<!-- <ul class="menu menu-horizontal p-0">
 			<li><a href="/about">About</a></li>
 			<li><a href="/contact">Contact</a></li>
-		</ul>
+		</ul> -->
 	</div>
 </nav>
 <main class="p-4">
